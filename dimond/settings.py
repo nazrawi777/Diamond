2a1dcb9331d306847de356e725e6926b8681b6d1
Chaos-19 <kalgetachew375@gmail.com>
2025-05-03 07:51:30 -0700

    Add STATIC_ROOT setting for static file collection during deployment

--- dimond/settings.py
+++ dimond/settings.py
@@ -123,6 +123,8 @@ STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'src', 'static'),
 ]
 
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')  # Directory to collect static files during deployment
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
